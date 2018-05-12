6916f1d4f63519727864773858af396a91090185
renaissains <renaissains@gmail.com>
2018-05-12 15:25:18 +0700

    fix removed App.js

---
+++ scrumbar/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+
+import './App.css';
+
+import store from './redux/store';
+import Home from './containers/Home';
+import CobaFirebase from './containers/CobaFirebase';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          <Home />
+          <CobaFirebase />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
