2ad6eacb03190aafb9a8b2b231357578f3d1c855
stedyyulius <stedyyulius@gmail.com>
2018-05-12 20:27:51 +0700

    tunggu bid

--- scrumbar/src/components/Navbar.js
+++ scrumbar/src/components/Navbar.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import { addTask } from '../helpers/task'
 
 class Navbar extends Component {
   constructor(props) {
     super(props)
-    this.state={}
+    this.state={
+      title: '',
+      desc: '',
+      type: ''
+    }
   }
+
+  createTask() {
+    const task = {
+      title: this.state.title,
+      desc: this.state.desc,
+      type: this.state.type,
+      status: 'Backlog',
+    }
+    addTask(task)
+  }
+
+
   render() {
     return (
       <div>
@@ -22,12 +39,21 @@ class Navbar extends Component {
                 </button>
               </div>
               <div className="modal-body">
-                <h6>Task Name</h6>
-                <input type="text" className="form-control" />
+                <h6>Name</h6>
+                <input type="text" className="form-control" onChange={(e) => this.setState({title: e.target.value})}/>
+                <br />
+                <h6>Description</h6>
+                <input type="text" className="form-control" onChange={(e) => this.setState({desc: e.target.value})}/>
+                <br />
+                <h6>Type</h6>
+                <select className="form-control" onChange={(e)=>this.setState({type: e.target.value})}>
+                  <option>Bug</option>
+                  <option>Feature</option>
+                </select>
               </div>
               <div className="modal-footer">
                 <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary">Create</button>
+                <button type="button" className="btn btn-primary" onClick={()=> this.createTask()}>Create</button>
               </div>
             </div>
           </div>
